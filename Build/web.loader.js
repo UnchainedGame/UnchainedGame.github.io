0a873c15fedd7acc2e1f38dc395f669ccb641c31
Hannah <hannah@harubex.com>
2024-03-05 14:48:32 -0700

    Merging

--- Build/web.loader.js
+++ Build/web.loader.js
@@ -55,7 +55,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 68027774,
+    wasmFileSize: 68027771,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
